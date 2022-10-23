c1a0eaa7564aa108a02c087eae1e6c52fc830367
Andrew <asundquistdavis@gmail.com>
2022-10-22 19:30:39 -0500

    readme.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # leaflet-challenge
-This is for the JS Lealflet challenge
+## Overview
+This project explores creating a website featuring a [Leaflet](https://leafletjs.com/) map. The site plots earhtquakes data, obtained from [USGS](https://earthquake.usgs.gov/), as features on in geojson.
+## Contents
+- leaflet-part-1
+    - static
+        -css
+            -styles.css: sets up necessary styling for leaflet
+        -js
+            - app.js: contains code to query geojson and populate map
+    - index.html: main page that displays earrthquake map
--- leaflet-part-1/static/js/app.js
+++ leaflet-part-1/static/js/app.js
@@ -1,13 +1,16 @@
+// init map
 let myMap = L.map("map", {
     center: [37.8283, -96.5795],
     zoom: 5});
   
-
+// add tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(myMap);
 
-  let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-  
+// define link
+let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// used to convert depth to color: color -> f(depth)
 function depthToColor(depth) {
   function fn(x) {return 1/(1+Math.exp(2.2*(.9-x/25)))}
   let red = 255*fn(depth);
@@ -15,20 +18,23 @@ function depthToColor(depth) {
   let blue = 50;
   return `rgb(${red} ,${green} , ${blue})`;};
 
+// used to convert magnitude to radius: radius -> g(magnitude)
 function magToRadius(mag) {
   return 10*Math.sqrt(Math.abs(mag))};
 
-  function style(feature) {
-    let depth = feature.geometry.coordinates[2];
-    let mag = feature.properties.mag;
-    return {
-    radius: magToRadius(mag),
-    fillColor: depthToColor(depth),
-    color: 'black',
-    weight: .3,
-    opacity: 1,
-    fillOpacity: 0.5};}
+// styyles feature: color -> f(depth), radius -> g(magnitude)
+function style(feature) {
+  let depth = feature.geometry.coordinates[2];
+  let mag = feature.properties.mag;
+  return {
+  radius: magToRadius(mag),
+  fillColor: depthToColor(depth),
+  color: 'black',
+  weight: .3,
+  opacity: 1,
+  fillOpacity: 0.5};}
 
+// fills in popup text for selected feature
 function popupText(feature) {
   let place = feature.properties.place;
   let mag = feature.properties.mag;
@@ -37,8 +43,10 @@ function popupText(feature) {
   return `<h5>Earthquake!</h5><hr></hr><p>This earthquake occurred on ${time}, ${place}, with a magnitude of ${mag}. More info can be found <a href="${url}">here</a>.</p>`;
 };
 
+// call data
 d3.json(link).then(function(earthquakes) {
 
+  // add geojason layer
   L.geoJson(earthquakes, {
     pointToLayer: function (feature, latlng) {
       return L
@@ -50,11 +58,11 @@ d3.json(link).then(function(earthquakes) {
   let legend = L.control({ position: "bottomright" });
   legend.onAdd = function() {
     let div = L.DomUtil.create("div", "info legend");
-    let depths = [...Array(111).keys()].map(depth=>depth-10)
+    let depths = [...Array(111).keys()].map(depth => depth - 10)
     let colors = depths.map(depth=>depthToColor(depth))
     let labels = [];
 
-    // Add the minimum and maximum.
+    // Add the minimum and maximum
     let legendInfo = "<h4>Depth of Earthquake (km)</h4>" +
       `<div class="labels"><div class="min">${depths[0]}</div><div class="max">${depths[depths.length-1]}</div></div>`;
 
